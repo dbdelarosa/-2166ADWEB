d516f4fc10a54296971b03cc07434d19ed56a668
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2024-01-11 15:55:36 +0800

    Add files via upload

---
+++ Lab Demo Data Binding/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_data_binding';
+  client_name = "Dennis Dela Rosa";
+  appliedCSS = 'green';
+  notApliedCSS = false;
+  myColor = 'blue';
+
+  clickCount=0
+  clickMe(){
+    this.clickCount++;
+    
+    }
+    resetClickCount(){
+      this.clickCount=0;
+  }
+}
